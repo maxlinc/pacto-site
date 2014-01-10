119509e4e582563cec4562c04693fde94cc239d2
Max Lincoln <max@devopsy.com>
2014-01-10 01:13:10 -0500

    Recurse over animation frames, so it'll be easier to add more content to the animation

--- src/documents/scripts/editor.js
+++ src/documents/scripts/editor.js
@@ -72,17 +72,37 @@ $(document).ready(function() {
     }
 
     var demoEditor = this;
-    typing = demoEditor.typeCode("# Welcome to Pacto\n# We're going to show you some basic usage here", 1000).promise();
-    typing.done(function() {
-      editor.setValue("");
-      demoEditor.typeCode("# First, add Pacto to your Gemfile\n\ngem 'pacto'\n", 1000).promise().done(function() {
+    function clearEditor() {
+      var deferred = new $.Deferred();
+      editor.setValue("")
+      deferred.resolve();
+      return deferred;
+    }
+    var animationFrames = [
+      function() { return demoEditor.typeCode("# Welcome to Pacto\n# We're going to show you some basic usage here", 1000) },
+      clearEditor,
+      function() { return demoEditor.typeCode("# First, add Pacto to your Gemfile\n\ngem 'pacto'\n", 1000) },
+      function() {
+        var deferred = new $.Deferred();
         panel = $("<div class=\"bubble\"></div>");
         panel.height($("#editor-section").height());
         panel.append("<h4>And then...</h4>");
         panel.append("<p>bundle install</p>");
-        $("#comments-container").append(panel).addClass('animated slideInRight');;
-      });
-    });
+        $("#comments-container").append(panel).addClass('animated slideInRight');
+        deferred.resolve();
+        return deferred;
+      }
+    ];
 
+    function runAnimation() {
+      var frame = animationFrames.shift();
+      if (typeof frame === "undefined") {
+        return
+      } else {
+        console.log("Running a frame");
+        frame().promise().done(runAnimation);
+      }
+    };
+    runAnimation();
   })();
 });
