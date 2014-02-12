674a6a090e9d6a0808b86718d4e2a6a21efa7a54
Max Lincoln <max@devopsy.com>
2014-02-12 13:35:21 -0500

    Update config samples

--- samples/configuration.rb
+++ samples/configuration.rb
@@ -12,7 +12,7 @@ Pacto.configure do |c|
   # If the request matching should be strict (especially regarding HTTP Headers).
   c.strict_matchers = true
   # You can set the Ruby Logger used by Pacto.
-  c.logger = Logger.instance
+  c.logger = Pacto::Logger.instance
   # (Deprecated) You can specify a callback for post-processing responses.  Note that only one hook
   # can be active, and specifying your own will disable ERB post-processing.
   c.register_hook do |contracts, request, response|
@@ -25,48 +25,9 @@ end
 # You can also do inline configuration.  This example tells the json-schema-generator to store default values in the schema.
 Pacto.configuration.generator_options = { :defaults => true }
 
-# We can load the contract and validate it, by sending a new request and making sure
-# the response matches the JSON schema.  Obviously it will pass since we just recorded it,
-# but if the service has made a change, or if you alter the contract with new expectations,
-# then you will see a contract validation message.
-contracts = Pacto.build_contracts('contracts', 'https://api.github.com')
-contracts.validate_all
-
-# We can also use Pacto to stub the service based on the contract.
-contracts.stub_all
-# The stubbed data won't be very realistic, the default behavior is to return the simplest data
-# that complies with the schema.  That basically means that you'll have "bar" for every string.
-readme = Octokit.readme 'thoughtworks/pacto'
-# You're now getting stubbed data.  Unless you generated the schema with the `defaults` option enabled,
-# then this will just return "bar" as the encoding.  If you recorded the defaults, then it will return
-# the value received when the Contract was generated.
-puts readme.type
-
-# You can turn on validation mode so Pacto will detect and validate HTTP requests.
-Pacto.validate!
-
-# Pacto comes with rspec matchers
+# If you're using Pacto's rspec matchers you might want to configure a reset between each scenario
 require 'pacto/rspec'
-describe 'my_code' do
-  it 'calls a service' do
-    Octokit.readme 'thoughtworks/pacto'
-    # The have_validated matcher makes sure that Pacto received and successfully validated a request
-    expect(Pacto).to have_validated(:get, 'https://api.github.com/repos/thoughtworks/pacto/readme')
-  end
-end
-
-# It's probably a good idea to reset Pacto between each rspec scenario
 RSpec.configure do |c|
   c.after(:each)  { Pacto.clear! }
 end
 
-contracts = Pacto.build_contracts('contracts', 'https://api.github.com').stub_all
-Pacto.validate!
-
-describe 'my_code' do
-  it 'calls a service' do
-    Octokit.readme 'thoughtworks/pacto'
-    # The have_validated matcher makes sure that Pacto received and successfully validated a request
-    expect(Pacto).to have_validated(:get, 'https://api.github.com/repos/thoughtworks/pacto/readme')
-  end
-end
